5b681c6c538ac4bee14f165c927dfd0882238df1
Matthias Hoefel <matthias@hoefel.net>
2019-10-19 15:28:01 +0200

    Umbau auf Java, Grundgerüst für angelegt.

---
+++ src/main/java/org/scc/samples/harrypotteronlinebookstore/Book.java
@@ -0,0 +1,13 @@
+package org.scc.samples.harrypotteronlinebookstore;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class Book {
+
+    private int bookId;
+    private String title;
+}
+
---
+++ src/main/java/org/scc/samples/harrypotteronlinebookstore/ShoppingCartServiceWithJava.java
@@ -0,0 +1,9 @@
+package org.scc.samples.harrypotteronlinebookstore;
+
+import java.util.List;
+
+class ShoppingCartServiceWithJava {
+    public int getDiscountedPrice(List<Book> books) {
+        return 0;
+    }
+}
